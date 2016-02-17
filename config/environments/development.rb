865e4c71e7e8c603b251cd2d2e8184b51bf537a5
Md. Tauhidul Islam <tauhidul35@gmail.com>
2016-02-18 00:55:01 +0600

    Install websocket and remove the Rack::Lock middleware

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Remove the Rack::Lock middleware for Websocket
+  config.middleware.delete Rack::Lock
 end
